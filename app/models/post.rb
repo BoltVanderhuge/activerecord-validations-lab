f6f34af61979838c4ca4dd40bc4068741497daa3
john_wisneski <john.j.wisneski@gmail.com>
2021-01-20 22:54:34 -0500

    commit tree 61f84138fc0e7e79c87ef34c46e48bccf2b22a1f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+    def clickbait?
+        click_bait_titles = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if title != nil 
+            # if click_bait_titles.include?(title) 
+            #     errors.add("title", "Must include 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'")
+            # end
+            if click_bait_titles.none? {|t| title.include?(t)}
+                errors.add("title", "Must include 'Won't Believe', 'Secret', 'Top [number]', or 'Guess'")
+            end
+        end
+    end
 end
